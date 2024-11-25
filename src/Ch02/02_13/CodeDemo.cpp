5fab8b596b0affb2bb977c9abcac250d7215708f
Erik Segura <158991388+Zatrayu84@users.noreply.github.com>
2024-11-25 18:27:57 +0000

    alternate solution, cleaner code

--- src/Ch02/02_13/CodeDemo.cpp
+++ src/Ch02/02_13/CodeDemo.cpp
@@ -8,9 +8,13 @@ int main(){
     int nums[5] = {1,23,32,24,337};
     float result; 
 
-    // Write your code here
-    result = ((float)(nums[0] + nums[1] + nums[2] + nums[3] + nums[4]) / 5);
+    // Write your code here - my way - I had to cast float due to integers being used to add and divide
+    //result = ((float)(nums[0] + nums[1] + nums[2] + nums[3] + nums[4]) / 5);
     
+    //Video Answer - no type casting needed because all gets stored in result which is already a float type
+    result = nums[0] + nums[1] + nums[2] + nums[3] + nums[4];
+    result /= 5;
+
     std::cout << "The average is " << result << std::endl;
     
     std::cout << std::endl << std::endl;
