9ee63e7493f1b72f3c354659985a8df941d4416b
Erik Segura <158991388+Zatrayu84@users.noreply.github.com>
2024-11-25 18:21:09 +0000

    enumerators and challenge done

--- src/Ch02/02_12b/CodeDemo.cpp
+++ src/Ch02/02_12b/CodeDemo.cpp
@@ -5,14 +5,16 @@
 #include <iostream>
 #include <cstdint>
 
-enum cow_purpose {dairy, meat, hide, pet};
+enum class cow_purpose {dairy, meat, hide, pet}; // numbering starts at [0, 1, 2, 3]
+enum class grocery_section {canned, frozen, meat, laundry, dairy, bakery}; // numbering starts at [0,1,2,3,4,5,6)
 
 int main(){
-    int a;
+    int meat = 8;
+    cow_purpose a;
 
-    a = meat;
+    a = cow_purpose::meat;
 
-    std::cout << "a = " << a << std::endl;
+    std::cout << "a = " << (int) a << std::endl; // insertion is not defined for operands of cow_purpose
 
     std::cout << std::endl << std::endl;
     return (0);
