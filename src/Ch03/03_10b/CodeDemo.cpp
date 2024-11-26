9d5ea7941b50de323792802ea54bdd38e1fc065d
Erik Segura <158991388+Zatrayu84@users.noreply.github.com>
2024-11-26 21:01:43 +0000

    Vectors are fun

--- src/Ch03/03_10b/CodeDemo.cpp
+++ src/Ch03/03_10b/CodeDemo.cpp
@@ -8,7 +8,19 @@
 #include "cow.h"
 
 int main(){
+
+    std::vector<cow> cattle;
+
+    cattle.push_back(cow("Betty", 6, cow_purpose::meat));
+    cattle.push_back(cow("Libby", 4, cow_purpose::hide));
+    cattle.push_back(cow("Trudy", 4, cow_purpose::pet));
+    cattle.push_back(cow("Shiela", 2, cow_purpose::dairy));
     
+    std::cout << "The first cow is " << cattle.begin()->get_name() << std::endl;
+    std::cout << "The first cow is " << cattle[1].get_name() << std::endl;
+    std::cout << "The first cow is " << prev(cattle.end(), 2)->get_name() << std::endl;
+    std::cout << "The first cow is " << (cattle.end() - 1)->get_name() << std::endl;
+
     std::cout << std::endl << std::endl;
     return (0);
 }
