e09b1587cc07fe660481814014f0830fceebdaa0
Erik Segura <158991388+Zatrayu84@users.noreply.github.com>
2024-11-26 21:23:03 +0000

    we got cows!!!

--- src/Ch03/03_10b/CodeDemo.cpp
+++ src/Ch03/03_10b/CodeDemo.cpp
@@ -16,10 +16,10 @@ int main(){
     cattle.push_back(cow("Trudy", 4, cow_purpose::pet));
     cattle.push_back(cow("Shiela", 2, cow_purpose::dairy));
     
-    std::cout << "The first cow is " << cattle.begin()->get_name() << std::endl;
-    std::cout << "The first cow is " << cattle[1].get_name() << std::endl;
-    std::cout << "The first cow is " << prev(cattle.end(), 2)->get_name() << std::endl;
-    std::cout << "The first cow is " << (cattle.end() - 1)->get_name() << std::endl;
+    std::cout << "  The first cow is " << cattle.begin()->get_name() << std::endl; // standard to pull first element
+    std::cout << "  The second cow is " << cattle[1].get_name() << std::endl; // standard to pull info by index
+    std::cout << "  The next to last cow is " << prev(cattle.end(), 2)->get_name() << std::endl; // prev command used to pull previous element from end of vector.
+    std::cout << "  The last cow is " << (cattle.end() - 1)->get_name() << std::endl; // this is to grab the last cow, must subtract one to get data, .end pulls last item past end of vector
 
     std::cout << std::endl << std::endl;
     return (0);
